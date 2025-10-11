e27a7322c74e7cec6d6727dfb3e9f7bb4afaeac2
dayanna2003-a <diananassoon@gmail.com>
2025-10-11 17:08:48 +0300

    YES

--- question.php
+++ question.php
@@ -1,2 +1,17 @@
-<?php 
-  require "views/question.view.php";
+<?php require "views/question.view.php";?>
+
+<?php session_start(); 
+  $question="PHP stands for what?";
+$choices=[
+  "personal hypertext processor",
+  "preprocessor hypretext processor",
+  "php hypertext preprocessor",
+  "private hypertext processor"
+];
+$answer="php hypertexr preprocessor";
+
+if($_SERVER['REQUEST_METHOD']){
+  
+}
+
+?>
--- views/question.view.php
+++ views/question.view.php
@@ -1,5 +1,11 @@
-<?php require "views/partials/header.php";?> 
+   <?php require "views/partials/header.php";?> 
    <div class="shadow p-5 bg-white">
       <h1 class="font-semibold text-3xl font-serif  md:pl-40">QUESTION</h1>
    </div>    
 <?php require "views/partials/end.php";?>
+
+<h2>Question 1:</h2><br>
+<p><?php echo $question;?></p>
+<form action ="" method="post">
+      <input type="radio" name="answer" value="<?php echo $choices[0];?>"><br>
+</form>
